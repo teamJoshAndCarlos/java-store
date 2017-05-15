9b40cb3272262c0531f7c77c9a4a4e728abf8da0
Carlos <c.soto93@hotmail.com>
2017-05-15 15:40:47 -0500

    Added continue shopping loop and print receipt functions (no total yet)

--- src/StoreApp.java
+++ src/StoreApp.java
@@ -2,49 +2,65 @@ import java.util.Scanner;
 
 public class StoreApp {
     public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Menu \n" +
-                "1) Sale \n" +
-                "2) Print Receipt \n" +
-                "3) Exit");
-
-        int option = input.nextInt();
-
-
         String ContinueShopping = "";
+        String receipt = "";
+        int option;
+        int itemNumber = 0;
+        int total = 0;
+        do {
+            System.out.println("Menu \n" +
+                    "1) Sale \n" +
+                    "2) Print Receipt \n" +
+                    "3) Exit");
+            option = input.nextInt();
 
-  do {
-      if(option == 1){
-          System.out.println(addToCart());
-          System.out.println("Continue Shopping? [Y/N]");
-          ContinueShopping = input.next();
-      } else if (option == 2) {
-          System.out.println("Print Receipt");
-      }else if(option == 3) {
-          System.out.println("Exit");
-      }else {
-          System.out.println("not valid");
-      }
-  }while (ContinueShopping.equalsIgnoreCase("Y"));
+            if (option == 1) {
+                do {
+                    itemNumber++;
+                    receipt += addToCart(itemNumber);
+                    System.out.println("Continue Shopping? [Y/N]");
+                    ContinueShopping = input.next();
+                } while (ContinueShopping.equalsIgnoreCase("Y"));
+            } else if (option == 2) {
+                System.out.println("Print Receipt");
+                System.out.println(receipt);
+                System.out.println("Total" + total);
+                option = 3;
+            } else if (option == 3) {
+                System.out.println("Exit");
+            } else {
+                System.out.println("not valid");
+            }
+
+        } while (option != 3);
+        System.out.println("Thank you come again!");
+
 
 //  if (ContinueShopping.equals("N")){
 //      System.out.println("Would you like to print your receipt? [Y/N]");
 //      int receipt = input.nextInt();
 //      System.out.println("Printing Receipt:" + addToCart());
-  }
+//    }
 
 
     }
 
 
-    public static String addToCart(){
+    public static String addToCart(int itemNumber) {
         System.out.println("What would you like to buy?");
         String item = input.next();
         System.out.println("How much does it cost?");
         int costOfItem = input.nextInt();
         System.out.println("How many");
         int numberOfItems = input.nextInt();
-        return ("1) " + item + " x" + numberOfItems + "  :$" + numberOfItems * costOfItem);
+        int total = numberOfItems * costOfItem;
+        return (itemNumber + ") " + item + " x" + numberOfItems + " :$" + total + "\n");
 
     }
+
+//    public static String printReceipt() {
+//
+//    }
 }
