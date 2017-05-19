e131d33d5c5cc433f61e83117b4023cbb81be7ea
JSR7 <G!thUb1!>
2017-05-19 12:20:48 -0500

    commit rectangle

---
+++ src/Java1/Rectangle.java
@@ -0,0 +1,32 @@
+package Java1;
+
+/**
+ * Created by jsr on 5/19/17.
+ */
+public class Rectangle {
+    public Rectangle() {
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+         double perimeter = 2*length + 2*width;
+
+         double area = length*width;
+
+
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+
+
+    private String length;
+    private String width;
+
+
+}
